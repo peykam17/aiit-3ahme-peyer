e78203c764e3919db2060c5d5d103a46c28f27fa
peykam17 <58695107+peykam17@users.noreply.github.com>
2020-05-11 13:15:01 +0200

    Delete TestFailedException.java

--- corona-java-peyer/ue03_serienschaltung/src/ue03_serienschaltung/TestFailedException.java
+++
@@ -1,9 +0,0 @@
-package ue02_serienschaltung;
-
-public class TestFailedException extends RuntimeException{
-
-    public TestFailedException(Throwable cause) {
-        super(cause);
-    }
-    
-}
